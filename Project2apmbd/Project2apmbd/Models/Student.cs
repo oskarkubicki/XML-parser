f554cee2f7f5288b20241a505b9ec95a4b7943ab
unknown <s19732@pjwstk.edu.pl>
2020-03-21 14:05:24 +0100

    models added

---
+++ Project2apmbd/Project2apmbd/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project2apmbd.Models
+{
+    class Student
+    {
+
+        public Student() { }
+
+        public Student(string name, string last, string email)
+        {
+
+            this.email = email;
+            this.firstname = name;
+            this.lastname = last;
+        }
+
+
+        public string firstname { get; set; }
+
+        public string lastname { get; set; }
+        public string birthdate { get; set; }
+        public string email { get; set; }
+
+        public string mothersname { get; set; }
+    }
+}
---
+++ Project2apmbd/Project2apmbd/Models/University.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Project2apmbd.Models
+{
+    public class University
+    {
+
+
+        public University()
+        {
+
+
+            CreatedAt = DateTime.Now.ToString();
+            Author = "Oskar Kubicki";
+        }
+        [XmlAttribute]
+
+        public string Author { get; set; }
+
+        [XmlAttribute(AttributeName = "createdAt")]
+        public string CreatedAt { get; set; }
+
+        HashSet<Student> Students { get; set; }
+    }
+}
